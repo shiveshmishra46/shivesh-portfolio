d6d9f9507911cf883ad5e0119b2ff9967b7403ab
Shivesh Mishra <133944808+shiveshmishra46@users.noreply.github.com>
2025-05-08 22:52:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Testimonials from "./sections/Testimonials";
-import Footer from "./sections/Footer";
-import Contact from "./sections/Contact";
-import TechStack from "./sections/TechStack";
-import ExperienceSection from "./sections/ExperienceSection";
-import Hero from "./sections/Hero";
-import Showcase from "./sections/Showcase";
+import Testimonials from "./sections/Testimonials.jsx";
+import Footer from "./sections/Footer.jsx";
+import Contact from "./sections/Contact.jsx";
+import TechStack from "./sections/TechStack.jsx";
+import ExperienceSection from "./sections/ExperienceSection.jsx";
+import Hero from "./sections/Hero.jsx";
+import Showcase from "./sections/Showcase.jsx";
 import LogoShowcase from "./sections/LogoShowcase.jsx";
-import FeatureCards from "./sections/FeatureCards";
-import Navbar from "./components/NavBar";
+import FeatureCards from "./sections/Featurecards.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => (
   <>
@@ -15,7 +15,7 @@ const App = () => (
     <Hero />
     <Showcase />
     <LogoShowcase />
-    <FeatureCards />
+    <Featurecards />
     <ExperienceSection />
     <TechStack />
     <Testimonials />
